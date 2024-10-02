d3ddcfaa23c232ea1675f51e250a1fed82738c15
David McNeil <dmcneil@equinix.com>
2024-10-02 09:13:54 -0500

    switch to use deps.edn

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject format-errors "v1"
-  :description "A Clojure module for producing formatted errors."
-  :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [prismatic/schema "1.4.0"]]
-  :profiles {:test [:test-dep]
-             :dev [:test-dep]})
